9cae49b1b9baf68ace35db66a77b6940a0bc705c
Guilherme <104802974+GuilhermeAdSilva@users.noreply.github.com>
2024-03-08 17:09:53 -0300

    cap03ex002

--- cap03ex002/main.c
+++ cap03ex002/main.c
@@ -4,13 +4,20 @@
 
 int main()
 {
+    //declaração de variáveis e configurações
     setlocale(LC_ALL, "Portuguese");
     float nota1, nota2, media;
+
+    //entrada
     printf("Informe a Nota 1: ");
     scanf("%f", &nota1);
     printf("Informe a Nota 2: ");
     scanf("%f", &nota2);
+
+    //processamento de dados
     media = (nota1 + nota2)/2;
+
+    //saída
     printf("Média: %f", media);
     return 0;
 }
