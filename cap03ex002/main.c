6cecebee0c7d42057b7bbbeb6232f537e4883e6a
Guilherme <104802974+GuilhermeAdSilva@users.noreply.github.com>
2024-03-08 17:06:17 -0300

    cap03ex02

---
+++ cap03ex002/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    float nota1, nota2, media;
+    printf("Informe a Nota 1: ");
+    scanf("%f", &nota1);
+    printf("Informe a Nota 2: ");
+    scanf("%f", &nota2);
+    media = (nota1 + nota2)/2;
+    printf("Média: %f", media);
+    return 0;
+}
